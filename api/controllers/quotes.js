5552510c218406f23d642b35fa97a2f2f3fde99a
Matt Lilley <m.k.lilley@gmail.com>
2022-08-03 21:18:27 +0100

    Adds async/await to quotesController

--- api/controllers/quotes.js
+++ api/controllers/quotes.js
@@ -2,27 +2,27 @@ const router = require("express").Router();
 
 const Quote = require("../models/Quote");
 
-router.get("/random", (req, res) => {
+router.get("/random", async (req, res) => {
     try {
-        const quote = Quote.showRandom();
+        const quote = await Quote.showRandom();
         res.json(quote);
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
 });
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const quotes = Quote.showAll();
+        const quotes = await Quote.showAll();
         res.json(quotes);
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
-        const quote = Quote.show(req.params.id);
+        const quote = await Quote.show(req.params.id);
         res.json(quote);
     } catch (e) {
         res.status(404).json({ error: e.message });
